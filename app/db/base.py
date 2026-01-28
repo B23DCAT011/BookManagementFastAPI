dfd9c540bf0e677831f1b95d4ed1fe915e456dd5
B23DCAT011 <ducanhluust@gmail.com>
2026-01-29 02:25:36 +0700

    Migration SQLite

---
+++ app/db/base.py
@@ -0,0 +1,8 @@
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+#import models for alembic
+from app.model.author import Author
+from app.model.book import Book
+from app.model.category import Category
---
+++ app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from app.api.endpoints import author,category,book
+
+app = FastAPI(
+    title="Book Management API",
+    description="Simple API to manage books,authors,category and book covers",
+    version="1.0.0"
+)
+
+#Include routes
+app.include_router(author.router,prefix="/author", tags=["Authors"])
+app.include_router(book.router,prefix="/book", tags=["Book"])
+app.include_router(category.router,prefix="/category", tags=["Category"])
+#Static files for covers image
+
+@app.get("/")
+def read_root():
+    return {"message" : "Book management API is running"}
